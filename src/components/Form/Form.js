4dd056697c490cd0948c275ee465940164a94b01
Sahana Balram <sahana2008balram@gmail.com>
2018-03-01 15:17:06 -0800

    Inital commit for the form

---
+++ src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    render() {
+        return (
+            <div>test</div>
+        );
+    }
+}
+
+export default Form;
