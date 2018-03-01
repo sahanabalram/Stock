c6bf33084f51bdbf2feb60a73383c4d611fcea9b
Sahana Balram <sahana2008balram@gmail.com>
2018-03-01 15:18:50 -0800

    Initial commit for titles component

---
+++ src/components/Titles/Titles.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Titles extends Component {
+    render() {
+        return(
+            <div>
+            <h1>Stock Finder</h1>
+            <h3>Find out about the different stocks</h3>
+            </div>
+        )
+    }
+}
+
+export default Titles;
