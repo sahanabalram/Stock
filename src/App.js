f9da89368d395fca393808a2ea6bc47fe0f4cd84
Sahana Balram <sahana2008balram@gmail.com>
2018-03-01 16:48:54 -0800

    Add inital state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import Stock from './components/Stocks';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // defining initial state
+    this.state = {
+      symbol: undefined,
+      companyName: undefined,
+      open: undefined,
+      high: undefined,
+      low: undefined
+    }
+  }
   render() {
     return (
       <div className="App">
