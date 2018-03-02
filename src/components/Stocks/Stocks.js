0b2a7910cd4c64cc407187937276bcd0e9d9f5c9
Sahana Balram <sahana2008balram@gmail.com>
2018-03-01 16:14:29 -0800

    Change test to stock component

--- src/components/Stocks/Stocks.js
+++ src/components/Stocks/Stocks.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Stock extends Component {
     render() {
         return(
-            <div>test</div>
+            <div>Stock Component</div>
         );
     }
 }
