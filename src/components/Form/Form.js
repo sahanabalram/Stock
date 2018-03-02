207f6d333bede45382bae4dfed321d9d2512e55f
Sahana Balram <sahana2008balram@gmail.com>
2018-03-01 16:14:09 -0800

    Change test to form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Form extends Component {
     render() {
         return (
-            <div>test</div>
+            <div>Form Component</div>
         );
     }
 }
